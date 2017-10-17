826ba7711de16925b9966e18d5fdd05c58ee2ee7
Victor Lopez <victor.lopez@pal-robotics.com>
2017-10-17 07:39:50 +0000

    Add gmock and gtest test packages

---
+++ gmock_test/test/gmock_test.cpp
@@ -0,0 +1,29 @@
+#include <gmock/gmock.h>
+
+using ::testing::Return;
+
+class Base
+{
+public:
+  virtual bool foo() = 0;
+};
+
+class MockDerived : public Base
+{
+public:
+  MOCK_METHOD0(foo, bool());
+};
+
+TEST(DummyTest, test)
+{
+  MockDerived bar;
+
+  EXPECT_CALL(bar, foo()).Times(1).WillOnce(Return(true));
+  EXPECT_TRUE(bar.foo());
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
